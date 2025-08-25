6d22f5cfec0c53987646bfe9f7816ee3de64409b
Marcus Nogueira <marcus.filho@memsolucoes.com.br>
2025-08-24 21:17:23 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
 import './App.css';
-import { ThemeProvider } from './context/ThemeContext';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
+  console.log('App component is rendering!');
+  
   return (
-    <ThemeProvider>
-      <div className="App">
-        <Header />
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <h1>Teste JavaScript</h1>
+      <p>Se você está vendo isso, o JavaScript está funcionando!</p>
+      <button onClick={() => alert('JavaScript funcionando!')}>
+        Clique aqui para testar
+      </button>
+    </div>
   );
 }
 
